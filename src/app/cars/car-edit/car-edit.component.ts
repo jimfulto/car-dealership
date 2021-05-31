229ed3ff7e60a225f6a68c66760a38886dedce86
Jim Fulto <jimfulto233@gmail.com>
2021-05-30 21:23:19 -0500

    created car edit component and form

--- src/app/cars/car-edit/car-edit.component.ts
+++ src/app/cars/car-edit/car-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { CarService } from '../car.service';
 
 @Component({
   selector: 'app-car-edit',
@@ -9,8 +11,9 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class CarEditComponent implements OnInit {
   id!: number;
   editMode = false;
+  carForm!: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private carService: CarService) { }
 
   ngOnInit(): void {
     this.route.params
@@ -18,8 +21,28 @@ export class CarEditComponent implements OnInit {
         (params: Params) => {
           this.id = +params['id'];
           this.editMode = params['id'] != null;
+          this.initForm();
         }
       );
+  }
+
+  private initForm() {
+    let carName = '';
+    let carImagePath = '';
+    let carDescription = '';
+
+    if(this.editMode) {
+      const car = this.carService.getCar(this.id);
+      carName = car.name;
+      carImagePath = car.imagePath;
+      carDescription = car.description;
+    }
+
+    this.carForm  = new FormGroup({
+      'name': new FormControl(carName),
+      'imagePath': new FormControl(carImagePath),
+      'description': new FormControl(carDescription)
+    });
   }
 
 }
